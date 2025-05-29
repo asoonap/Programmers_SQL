744e445a52e4dc8cb2c66580571b7884f61ef646
asoonap <namminju0328@gmail.com>
2025-05-29 17:02:40 +0900

    join - <L4>특정 기간동안 대여 가능한 자동차들의 대여비용 구하기

---
+++ join/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -0,0 +1,32 @@
+-- CAR_RENTAL_COMPANY_CAR : 대여중 차 정보
+-- CAR_RENTAL_COMPANY_RENTAL_HISTORY : 대여 기록
+-- CAR_RENTAL_COMPANY_DISCOUNT_PLAN : 차종별 대여기간 종류별 할인
+
+-- CAR_RENTAL_COMPANY_CAR 테이블과 CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블과 CAR_RENTAL_COMPANY_DISCOUNT_PLAN 테이블에서 
+-- 자동차 종류가 '세단' 또는 'SUV' 인 자동차 중
+-- 2022년 11월 1일부터 2022년 11월 30일까지 대여 가능하고 
+-- 30일간의 대여 금액이 50만원 이상 200만원 미만인 자동차에 대해서
+-- 자동차 ID, 자동차 종류, 대여 금액(컬럼명: FEE) 리스트를 출력하는 SQL문을 작성해주세요.
+-- 결과는 대여 금액을 기준으로 내림차순 정렬하고,
+-- 대여 금액이 같은 경우 자동차 종류를 기준으로 오름차순 정렬, 자동차 종류까지 같은 경우 자동차 ID를 기준으로 내림차순 정렬해주세요.
+
+SELECT A.CAR_ID, A.CAR_TYPE, ROUND(A.DAILY_FEE *(1- (DISCOUNT.DISCOUNT_RATE/100))* 30) AS FEE
+FROM 
+    (SELECT CAR.CAR_ID, CAR.CAR_TYPE, CAR.DAILY_FEE
+        FROM CAR_RENTAL_COMPANY_CAR  CAR 
+        JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY HISTORY 
+        ON CAR.CAR_ID = HISTORY.CAR_ID
+        WHERE CAR.CAR_TYPE REGEXP ('세단|SUV')
+        And HISTORY.END_DATE <= '2022-11-01' OR HISTORY.START_DATE >= '2022-12-01')
+        AS A
+    JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS DISCOUNT
+    ON A.CAR_TYPE = DISCOUNT.CAR_TYPE
+    WHERE DISCOUNT.DURATION_TYPE LIKE '30%'
+    AND (A.DAILY_FEE *(1- (DISCOUNT.DISCOUNT_RATE*0.01))* 30) >= 500000
+    AND (A.DAILY_FEE *(1 - (DISCOUNT.DISCOUNT_RATE*0.01))* 30) < 2000000
+    GROUP BY CAR_ID
+    ORDER BY FEE DESC, CAR_TYPE, CAR_ID DESC
+
+-- 정답x - (날짜 부분이 잘못됨)
+-- HISTORY.END_DATE <= '2022-11-01' OR HISTORY.START_DATE >= '2022-12-01')
+-- 머리 아프니 내일 해결...
