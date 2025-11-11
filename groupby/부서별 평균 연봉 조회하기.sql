d931ce4a937fce1f4f076675cee0ec8b7d4f87e4
asoonap <namminju0328@gmail.com>
2025-11-11 22:43:59 +0900

    groupby - <L3> 부서별 평균 연봉 조회하기

---
+++ groupby/부서별 평균 연봉 조회하기.sql
@@ -0,0 +1,28 @@
+-- 부서별 평균 연봉 조회하기
+-- HR_DEPARTMENT와 HR_EMPLOYEES 테이블을 이용해 부서별 평균 연봉을 조회하려 합니다.
+-- 부서별로 부서 ID, 영문 부서명, 평균 연봉을 조회하는 SQL문을 작성해주세요.
+-- 평균연봉은 소수점 첫째 자리에서 반올림하고 컬럼명은 AVG_SAL로 해주세요.
+-- 결과는 부서별 평균 연봉을 기준으로 내림차순 정렬해주세요.
+
+SELECT E.DEPT_ID, D.DEPT_NAME_EN, E.AVG_SAL
+FROM HR_DEPARTMENT D 
+JOIN (
+    SELECT DEPT_ID, ROUND(AVG(SAL)) AS AVG_SAL
+    FROM HR_EMPLOYEES
+    GROUP BY DEPT_ID
+    ) E
+    ON D.DEPT_ID = E.DEPT_ID
+ORDER BY AVG_SAL DESC
+
+
+-- CTE를 사용한 쿼리
+WITH EMPLOYEES_AVG_SAL AS (
+    SELECT DEPT_ID, ROUND(AVG(SAL)) AS AVG_SAL
+    FROM HR_EMPLOYEES
+    GROUP BY DEPT_ID)
+
+SELECT A.DEPT_ID, B.DEPT_NAME_EN,A.AVG_SAL
+FROM EMPLOYEES_AVG_SAL A
+    JOIN HR_DEPARTMENT B
+    ON A.DEPT_ID = B.DEPT_ID
+ORDER BY AVG_SAL DESC
